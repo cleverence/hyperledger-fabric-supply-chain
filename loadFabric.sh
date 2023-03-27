665cdc9da1ac8947157d5accd1671a9eefe68b92
cleverence <clevernce@gmail.com>
2023-03-27 22:23:24 +0300

    update on version

--- loadFabric.sh
+++ loadFabric.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Blockchain Technology Bootcamp Training with Cleverence Kombe
 # Author: Cleverence Kombe
-# load fabric images 2.1.0 fabric-ca 1.4.7 fabic-couchdb 0.4.20
-curl -sSL https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh | bash -s -- 2.1.0 1.4.7 0.4.20
+# load fabric images 2.3.2 fabric-ca 1.5.0 fabic-couchdb 0.4.20
+curl -sSL https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh | bash -s -- 2.3.2 1.5.0 0.4.20
 echo "========= download completed =========== "
 cp -R fabric-samples/bin .
 cp -R fabric-samples/config .
